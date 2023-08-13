4c8b657632d2ef31faa7a0f81e84c801392e3115
Hleb Drazdou <Hleb.Drazdou@gmail.com>
2023-08-13 19:21:09 +0200

    added logic to floor for destroy other.gameObjects

--- Assets/Scripts/Game/Floor.cs
+++ Assets/Scripts/Game/Floor.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 namespace Arkanoid.Game
 {
+    [RequireComponent(typeof(Collider2D))]
     public class Floor : SingletonMonoBehaviour<Floor>
     {
         #region Unity lifecycle
 
-        private void OnCollisionEnter2D(Collision2D other)
+        private void Awake()
         {
-            GameService.Instance.OnFloorHit();
+            GetComponent<Collider2D>().isTrigger = true;
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.CompareTag(Tags.Ball))
+            {
+                GameService.Instance.BallHitFloor();
+            }
+            else
+            {
+                Destroy(other.gameObject);
+            }
         }
 
         #endregion
